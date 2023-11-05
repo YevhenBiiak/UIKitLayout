30c21c0ebc6ac53a4a83638f774a9a5a827ae432
macbook <yevhen.biiak@icloud.com>
2023-11-05 23:03:28 +0200

    add buildExpression(_ expression: Void) -> [UIView] { [] } to StackViewBuilder

--- Sources/UIKitLayout/UIStackView/UIStackView + init.swift
+++ Sources/UIKitLayout/UIStackView/UIStackView + init.swift
@@ -12,6 +12,7 @@ public struct StackViewBuilder {
     public static func buildBlock(_ components: UIView...) -> [UIView] { components }
     public static func buildBlock(_ components: [UIView]...) -> [UIView] { components.flatMap { $0 } }
     public static func buildArray(_ components: [[UIView]]) -> [UIView] { components.flatMap { $0 } }
+    public static func buildExpression(_ expression: Void) -> [UIView] { [] }
     public static func buildExpression(_ expression: UIView) -> [UIView] { [expression] }
     public static func buildOptional(_ component: [UIView]?) -> [UIView] { component ?? [] }
     public static func buildEither(first component: [UIView]) -> [UIView] { component }
